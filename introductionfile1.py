5082bf68a1288e4ce2fe9840d5b60bcdc8a9bb36
Mokshada Mali <mokshadamali07@gmail.com.com>
2026-07-15 10:36:19 +0530

    my first comment

---
+++ introductionfile1.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+proffesion = input("Enter your profession: ")
+
+def introduction(name,age,proffesion):
+    print("\n good morning!")
+    print("my name is ",name)
+    print("my age is ",age)
+    print("my profession is ",proffesion)
+
+introduction(name,age,proffesion)
---
+++ studentinfofile.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+division =input("Enter your division: ")
+proffesion = input("Enter your profession: ")
+
+
+def introduction(name, division, proffesion, year="SE"):
+    print("My name is",name)
+    print("My division is",division)
+    print("MY profession is",proffesion)
+    print("i am currently studying in year",year)
+
+
+introduction(name,division,proffesion)
